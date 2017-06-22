1b612e31bf511ef9d627e18530e757d3b6187c4a
ddd <724987481@qq.com>
2017-06-22 17:53:22 +0800

    新增swift／OC文件模版

---
+++ HexoTemplates/boyer.xctemplate/Objective-C/___FILEBASENAME___.m
@@ -0,0 +1,42 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___(github地址)
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAMEASIDENTIFIER___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - def
+
+//MARK: override
+
+//TODO: - 将要实现
+
+//FIXME: - dddd
+
+
+#pragma mark - override
+
+
+#pragma mark - api
+
+
+#pragma mark - model event
+
+
+#pragma mark - view event
+
+
+
+#pragma mark - private
+
+
+
+#pragma mark - getter / setter
+
+@end
+
---
+++ HexoTemplates/boyer.xctemplate/Swift/___FILEBASENAME___.swift
@@ -0,0 +1,33 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___(github地址)
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___
+{
+    
+    //MARK:- override
+    
+    //MARK:- api
+    
+    //MARK:- model event
+    
+    
+    //MARK:- view event
+    
+    
+    //MARK:- private
+    
+    //MARK:-  getter / setter
+    
+    //TODO: - 将要实现
+    
+    //FIXME: - dddd
+}
+
+
