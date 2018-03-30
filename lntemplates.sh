323af9f0de6fdcf0d74e26fef2e42530c6e2082f
Your Name <you@example.com>
2018-03-30 18:10:20 +0800

    修复安装xcode模版时，目录不存在的问题

--- lntemplates.sh
+++ lntemplates.sh
@@ -7,6 +7,11 @@
 #  Copyright © 2017 hexo主题. All rights reserved.
 
 SRC_HOME=`pwd`
-echo "ln -fs ${SRC_HOME}/Util/HexoTemplates ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD"
+
+XcodePATH="~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD"
+echo "mkdir -p ${XcodePATH}"
+mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
+
+echo "ln -fs ${SRC_HOME}/Util/HexoTemplates ${XcodePATH}"
 ln -fs ${SRC_HOME}/Util/HexoTemplates ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
 echo "安装hexo模版完成"
