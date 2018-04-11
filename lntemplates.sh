11dc1c50df7715e266b6830dd430c843cb42b21b
likuan <377509050@qq.com>
2018-04-11 19:14:45 +0800

    整理项目工程模版

--- lntemplates.sh
+++ lntemplates.sh
@@ -11,7 +11,9 @@ SRC_HOME=`pwd`
 XcodePATH="~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD"
 echo "mkdir -p ${XcodePATH}"
 mkdir -p ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
+mkdir -p ~/Library/Developer/Xcode/Templates/Project\ Templates/ProjectTemplates
 
 echo "ln -fs ${SRC_HOME}/Util/HexoTemplates ${XcodePATH}"
 ln -fs ${SRC_HOME}/Util/HexoTemplates ~/Library/Developer/Xcode/Templates/File\ Templates/HexoMD
+ln -fs ${SRC_HOME}/Util/HexoTemplates/ProjectTemplates ~/Library/Developer/Xcode/Templates/Project\ Templates/ProjectTemplates
 echo "安装hexo模版完成"
